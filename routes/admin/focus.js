9167ea33069ca131253f3bfc48d432f25c3425ed
kongchuishan <cshan.kong@gmail.com>
2018-12-17 08:53:17 +0800

    feat: 新增轮播图列表页面

--- routes/admin/focus.js
+++ routes/admin/focus.js
@@ -1,12 +1,29 @@
 // 轮播图的增加修改删除
 const router = require('koa-router')()
+const tools = require('../../module/tools'),
+      DB = require('../../module/db')
 
-router.get('/', async (ctx) => {
-  await ctx.render('admin/focus/index')
+router.get('/',async (ctx)=>{
+
+  var page=ctx.query.page ||1;
+  var pageSize=3;
+  var result= await DB.find('focus',{},{},{
+      page,
+      pageSize
+  });
+  var count= await  DB.count('focus',{});  /*总数量*/
+  await  ctx.render('admin/focus/list',{
+      list:result,
+      page:page,
+      totalPages:Math.ceil(count/pageSize)
+  });
 })
 
-router.get('/add', async (ctx) => {
-  await ctx.render('admin/focus/add')
+
+router.get('/add',async (ctx)=>{
+
+  ctx.body="增加轮播图";
+
 })
 
 router.get('/edit', async (ctx) => {
